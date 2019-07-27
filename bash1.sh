218a80d8b0a9407e5b774933a53728d3b1142d45
Gowtham <gowthamraj198@gmail.com>
2019-07-27 15:53:12 +0530

    added jmeter tests in pipelines

--- bash1.sh
+++ bash1.sh
@@ -3,5 +3,5 @@ sleep 20
 mvn test -Papi-tests
 mvn -DThreadCount=1000 -DTimeOut=3 verify -Papi-performace-tests
 
-#kill -9 $(lsof -t -i :7338 -s tcp:LISTEN)
+kill -9 $(lsof -t -i :7338 -s tcp:LISTEN)
 exit
