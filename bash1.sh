7517ddd061548333aa5cbc1872d35a4a7416431a
Gowtham <gowthamraj198@gmail.com>
2019-07-27 15:10:23 +0530

    added jmeter tests in pipelines

--- bash1.sh
+++ bash1.sh
@@ -1,6 +1,7 @@
 nohup mvn spring-boot:run &
 sleep 20
 mvn test -Papi-tests
+mvn -DThreadCount=1000 -DTimeOut=3 verify -Papi-performace-tests
 
 #kill -9 $(lsof -t -i :7338 -s tcp:LISTEN)
-#exit
+exit
